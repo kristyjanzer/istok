390f76e5c941c32588bb18a18e3e7b5d0eb47f88
Kristina Yancer <kristyjanzer.work@gmail.com>
2024-10-08 20:18:22 +0300

    first

---
+++ js/common.js
@@ -0,0 +1,19 @@
+$(function() {
+
+});
+
+// Burger Menu
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const burgerMenu = document.querySelector(".header .header-button-menu");
+const burgerBody = document.querySelector(".header .header-button-menu__body");
+
+
+burgerMenu.addEventListener("click", (e) => {
+  header.classList.toggle("header-menu-open");
+  burgerBody.classList.toggle("active");
+  body.classList.toggle("lock");
+  e.stopPropagation();
+});
+
+$(".header-info__columns").clone().appendTo(".header-button-menu__body");
